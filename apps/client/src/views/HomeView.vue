578134e39ecfe4bcce0b9c2fbad533b469cc05a8
Panjkrc <panjkrc.mihael@gmail.com>
2022-05-01 16:48:14 +0200

    Update HomeView.vue

--- apps/client/src/views/HomeView.vue
+++ apps/client/src/views/HomeView.vue
@@ -1,15 +1,3 @@
 <template>
-  <CommentComponent :comment="comment" />
+  <router-link class="link underline" to="/register">register</router-link>
 </template>
-
-<script setup>
-import CommentComponent from "@/components/CommentComponent.vue";
-let comment = {
-  user: {
-    username: "John Doe",
-  },
-  created_at: "2020-01-01",
-  content:
-    "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur euismod, nisl nisi porta nisl, euismod euismod nisi nisi euismod.</p>",
-};
-</script>
