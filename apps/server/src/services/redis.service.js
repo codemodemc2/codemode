e74fcafc3c8f5d609d45197c525a29df4ac70733
Panjkrc <panjkrc.mihael@gmail.com>
2022-05-02 19:38:02 +0200

    Update redis.service.js

--- apps/server/src/services/redis.service.js
+++ apps/server/src/services/redis.service.js
@@ -1,13 +1,17 @@
 const { createClient } = require("redis");
 
-let redisClient = createClient({
-  legacyMode: true,
-  //password: process.env.REDIS_PASSWORD,
-});
+let config = {
+	legacyMode: true,
+};
+
+if( process.env.NODE_ENV === "production") config.password = process.env.REDIS_PASSWORD;
+
+
+let redisClient = createClient(config);
 
 module.exports = {
-  client: redisClient,
-  async connect() {
-    await redisClient.connect();
-  },
+	client: redisClient,
+	async connect() {
+		await redisClient.connect();
+	},
 };
