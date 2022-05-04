227153f4bf0a2b688f3fb6d53a9b04a6707f0a9c
Panjkrc <panjkrc.mihael@gmail.com>
2022-05-04 22:12:53 +0200

    invite link database schema

--- apps/server/src/database/models/invite_link.js
+++ apps/server/src/database/models/invite_link.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const InviteLinkSchema = new mongoose.Schema(
   {
     id: String,
+	created_by: mongoose.Types.ObjectId,
+	company_id: mongoose.Types.ObjectId,
   },
   { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
 );
