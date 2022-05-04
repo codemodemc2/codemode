8f3b7f7af02c56a11bf8c5467891ea8b46dcf2fa
Panjkrc <panjkrc.mihael@gmail.com>
2022-05-04 22:14:24 +0200

    added error toast

---
+++ apps/client/src/helpers/toast.js
@@ -0,0 +1,38 @@
+import { useToast } from "vue-toastification";
+
+const toast = useToast();
+
+
+export const errorToast = (error) => {
+	toast.error(error, {
+		position: "top-center",
+		timeout: 5000,
+		closeOnClick: false,
+		pauseOnFocusLoss: false,
+		pauseOnHover: false,
+		draggable: false,
+		draggablePercent: 0.6,
+		showCloseButtonOnHover: false,
+		hideProgressBar: true,
+		closeButton: "button",
+		icon: false,
+		rtl: false,
+	});
+};
+
+export const successToast = (error) => {
+	toast.error(error, {
+		position: "top-center",
+		timeout: 5000,
+		closeOnClick: false,
+		pauseOnFocusLoss: false,
+		pauseOnHover: false,
+		draggable: false,
+		draggablePercent: 0.6,
+		showCloseButtonOnHover: false,
+		hideProgressBar: true,
+		closeButton: "button",
+		icon: false,
+		rtl: false,
+	});
+};
--- apps/client/src/main.js
+++ apps/client/src/main.js
@@ -1,11 +1,16 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import { plugin as VueTippy } from 'vue-tippy';
+import 'tippy.js/dist/tippy.css';
 
 import App from "./App.vue";
 import router from "./router";
 import "@/assets/base.css";
-import "@/helpers/axios_config";
+import "@/helpers/axios_config.js";
+
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
@@ -14,6 +19,12 @@ const app = createApp(App);
 
 app.use(pinia);
 app.use(router);
+app.use(VueTippy);
+app.use(Toast, {
+	transition: "Vue-Toastification__fade",
+	maxToasts: 3,
+	newestOnTop: true,
+});
 
 app.mount("#app");
 
