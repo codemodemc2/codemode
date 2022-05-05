fda6142948c4e9132b5e60bb33ae71a7962c883c
Panjkrc <panjkrc.mihael@gmail.com>
2022-05-05 15:59:42 +0200

    Update admin_onboarding.js

--- apps/client/src/stores/admin_onboarding.js
+++ apps/client/src/stores/admin_onboarding.js
@@ -43,6 +43,7 @@ export const useAdminOnboardingStore = defineStore("admin_onboarding", {
 	actions: {
 		async register() {
 			let response = await registerAdmin(this, this.steps[1].data.email, this.steps[2].data.password);
+			console.log(response);
 			userStore.session.logged_in = true;
 			userStore.session.session_expires_at = Date.now() + response.expires_in * 1000;
 			userStore.user_data = response.user;
