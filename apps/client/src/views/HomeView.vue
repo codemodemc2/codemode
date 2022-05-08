d35dd6a7861ab9ec120438f1af9fdda2f160e1b1
Panjkrc <panjkrc.mihael@gmail.com>
2022-05-08 20:39:34 +0200

    Update HomeView.vue

---
+++ apps/client/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <component :is="c"></component>
+</template>
+
+<script setup>
+import { useUserStore } from "@/stores/user.js";
+import { ref } from "vue";
+import NotLoggedInView from "@/views/NotLoggedInView.vue";
+import FrontPageView from "@/views/FrontPage/FrontPageView.vue";
+
+let userStore = useUserStore();
+
+let c = ref(null);
+if (userStore.session.logged_in) {
+  c = FrontPageView;
+} else {
+  c = NotLoggedInView;
+}
+</script>
