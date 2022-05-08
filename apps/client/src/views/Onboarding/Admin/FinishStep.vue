3a52bd05266b2bfa3fbd77709f0270beb4d2a22e
Panjkrc <panjkrc.mihael@gmail.com>
2022-05-08 16:54:21 +0200

    try fix

--- apps/client/src/views/Onboarding/Admin/FinishStep.vue
+++ apps/client/src/views/Onboarding/Admin/FinishStep.vue
@@ -1,15 +1,5 @@
 <template>
-  <TransitionRoot
-    appear
-    :show="show"
-    class="flex flex-col h-full w-full content-center justify-center"
-    enter="transition-all duration-500"
-    enter-from="opacity-0 translate-x-96"
-    enter-to="opacity-100 translate-y-0"
-    leave="transition-all duration-500"
-    leave-from="opacity-100"
-    leave-to="opacity-0 -translate-x-96"
-  >
+  <div v-if="show" class="flex flex-col h-full w-full content-center justify-center">
     <div
       class="flex flex-col h-full w-1/3 content-center justify-center self-center gap-5 max-w-max px-5"
     >
@@ -21,39 +11,30 @@
         class="w-10 h-10 rotate-180 animate-spin self-center text-brand-primary"
       />
     </div>
-  </TransitionRoot>
-  <TransitionRoot
-    appear
-    :show="registered"
-    class="flex flex-col h-full w-full content-center justify-center"
-    enter="transition-all duration-500"
-    enter-from="opacity-0 translate-x-96"
-    enter-to="opacity-100 translate-y-0"
-    leave="transition-all duration-500"
-    leave-from="opacity-100"
-    leave-to="opacity-0 -translate-x-96"
-  >
+  </div>
+  <div v-if="registered" class="flex flex-col h-full w-full content-center justify-center">
     <div
       class="flex flex-col h-full w-1/3 content-center justify-center self-center gap-5 max-w-max px-5"
     >
       <p class="text-3xl text-brand-medium self-center font-bold">
         You are all set!
       </p>
-      <p class="">
-        Continue to the Admin panel to start managing your team.
-      </p>
-      <button class="submit-button mt-5" @click="$router.push({ path: '/dashboard' })">
+      <p class="">Continue to the Admin panel to start managing your team.</p>
+      <button
+        class="submit-button mt-5"
+        @click="$router.push({ path: '/dashboard' })"
+      >
         Go to dashboard ->
       </button>
     </div>
-  </TransitionRoot>
+  </div>
 </template>
 
 <script setup>
 import { onBeforeUnmount, onMounted, ref } from "vue";
 import { useAdminOnboardingStore } from "@/stores/admin_onboarding.js";
 import { RefreshIcon } from "@heroicons/vue/outline";
-import { TransitionRoot } from "@headlessui/vue";
+import { div } from "@headlessui/vue";
 
 const adminOnboardingStore = useAdminOnboardingStore();
 
