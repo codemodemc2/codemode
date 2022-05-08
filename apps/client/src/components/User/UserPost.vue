bdbe48a68365373860f4bcdc4bbee1049fd53836
HUNTERANGER <102363132+HUNTERANGER@users.noreply.github.com>
2022-05-08 20:15:47 +0200

    post profil neke osobe + navigation bar komponenta
    post profil neke osobe + navigation bar komponenta

---
+++ apps/client/src/components/User/UserPost.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col gap-1 border-2 rounded-xl px-5 py-2 border-gray-800 m-2">
+    <div class="flex flex-row gap-2">
+      <p>{{ post.username }}</p>
+      •
+      <p class="font-bold">{{ post.time }}</p>
+      •
+      <p><span class="font-bold">{{ post.likes }}</span> likes</p>
+    </div>
+    <div>
+      <p class="font-medium text-gray-800">{{ post.title }}</p>
+    </div>
+    <div>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  post: Object,
+});
+</script>
--- apps/client/src/views/HomeView.vue
+++ apps/client/src/views/HomeView.vue
@@ -1,3 +1,46 @@
 <template>
-  <router-link class="link underline" to="/register">register</router-link>
+  <navigation-bar/>
+  <div class="mx-60 my-5">
+    <user-header :profile="profile"/>
+    <div class="flex flex-row px-2 py-2">
+      <button class="border-2 px-12 py-3 border-gray-800 my-4" style="border-radius: 0.75rem 0 0 0.75rem;border-right-width:1px;">Ideas</button>
+      <button class="border-2 px-8 py-3 border-gray-800 my-4" style="border-radius: 0 0.75rem 0.75rem 0;border-left-width:1px;">Comments</button>
+      <form class="searchform flex flex-row gap-1 p-1 w-[100%]" action="">
+        <select name="sort" id="sort" class="border-2 py-2 border-gray-800 rounded-xl" style="margin:1rem 0 1rem 85%">
+          <option value="recent">Sort by recent</option>
+          <option value="liked">Sort by most liked</option>
+          <option value="alphabet">Sort alphabeticaly</option>
+        </select>
+      </form>
+    </div>
+    <div class="flex flex-col gap-1 border-2 rounded-xl px-4 py-8 border-gray-800 m-2">
+    <user-post :post="post" />
+    <user-post :post="post" />
+    <user-post :post="post" />
+    <user-post :post="post" />
+    </div>
+  </div>
 </template>
+<script setup>
+import {ref} from "vue"
+let post=ref({
+  title:"Lore ipsum",
+  body:"ipsum dolor sit amet",
+  username:"fdasfsdf",
+  time:"13h",
+  likes:320,
+})
+let profile=ref({
+  name:"John Doe",
+  username:"Johnny.Donny",
+  points:200,
+  posts:13,
+  comments:320,
+  rank:"3rd",
+  position:"unknown",
+})
+import NavigationBar from "@/components/NavigationBar.vue"
+import UserHeader from "@/components/User/UserHeader.vue"
+import UserPost from "@/components/User/UserPost.vue"
+
+</script>
