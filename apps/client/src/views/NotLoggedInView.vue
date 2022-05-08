00cab4a911c855b51ed299b7ec2a1033958d9ec8
Panjkrc <panjkrc.mihael@gmail.com>
2022-05-08 20:52:14 +0200

    Turn of build sourcemap

--- apps/client/src/views/NotLoggedInView.vue
+++ apps/client/src/views/NotLoggedInView.vue
@@ -1,4 +1,6 @@
 <template>
-  <router-link class="link underline" to="/register">register</router-link>
-  <router-link class="link underline" to="/login">login</router-link>
+  <div class="flex flex-col gap-3 p-5">
+    <router-link class="link underline" to="/register">register</router-link>
+    <router-link class="link underline" to="/login">login</router-link>
+  </div>
 </template>
--- apps/client/vite.config.js
+++ apps/client/vite.config.js
@@ -10,8 +10,5 @@ export default defineConfig({
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
 		},
-	},
-	build: {
-		sourcemap: true,
-	},
+	}
 });
