35befcedf0d2c3767f34e9115c5c98eb70d84e27
Lovro <61094400+LovroRogina@users.noreply.github.com>
2022-05-19 19:48:37 +0200

    Promjenjena pozadina

--- apps/client/src/views/FrontPage/FrontPageView.vue
+++ apps/client/src/views/FrontPage/FrontPageView.vue
@@ -1,10 +1,21 @@
 <template>
+
   <div class="flex flex-col h-screen relative">
     <div class="w-screen sticky top-0 z-50">
       <NavigationBarVue />
     </div>
-
-    <router-view></router-view>
+    <div
+      class="home-bg absolute top-0 h-full z-0 w-full bg-fixed blur-[1px] filter"
+    />
+    <div
+      class="absolute top-0 z-10 h-full w-full bg-gradient-to-br from-transparent via-gray-50 to-gray-100 bg-fixed"
+    />
+<div
+            class="absolute -left-60 top-40 z-10 h-[50vh] w-[200vw] -rotate-[10deg] transform bg-gradient-to-r from-sky-400 to-sky-600"
+          ></div>
+    <router-view class="z-50">
+      
+    </router-view>
   </div>
 </template>
 
