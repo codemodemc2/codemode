c47269abfd652d7b11b67c97eecd141c23d2cf1b
Panjkrc <panjkrc.mihael@gmail.com>
2022-05-23 20:35:59 +0200

    Update CommentListing.vue

--- apps/client/src/components/Comments/CommentListing.vue
+++ apps/client/src/components/Comments/CommentListing.vue
@@ -6,18 +6,19 @@
         :key="parent._id"
         class="flex flex-col gap-4"
       >
-        <CommentComponent :comment="parent" />
+        <CommentComponent :comment="parent" @refresh="refreshComments"/>
       </div>
     </div>
   </div>
   <div v-else class="flex flex-col items-center justify-center py-10">
     <p class="text-brand-dark text-xl">No comments yet.</p>
-    <p class="text-brand-dark text-xl">Be the first one to comment on this idea!</p>
-		
+    <p class="text-brand-dark text-xl">
+      Be the first one to comment on this idea!
+    </p>
   </div>
 </template>
 <script setup>
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import { getComments } from "@/helpers/api/comment.js";
 import { errorToast } from "@/helpers/toast.js";
 import CommentComponent from "./CommentComponent.vue";
@@ -26,7 +27,7 @@ let props = defineProps(["postId"]);
 
 let comments = ref([]);
 
-(async () => {
+let refreshComments = async () => {
   try {
     let res = await getComments(props.postId);
     comments.value = res.data.comments;
@@ -34,5 +35,11 @@ let comments = ref([]);
     console.log(error);
     errorToast("Error fetching comments");
   }
-})();
+};
+
+refreshComments();
+
+defineExpose({
+	refreshComments,
+});
 </script>
