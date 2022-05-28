68f1d5f4dc10b11cd744ffb02b9f5096ffa4392a
Panjkrc <panjkrc.mihael@gmail.com>
2022-05-28 12:04:24 +0200

    Update FrontPageListingView.vue

--- apps/client/src/views/FrontPage/FrontPageListingView.vue
+++ apps/client/src/views/FrontPage/FrontPageListingView.vue
@@ -11,13 +11,13 @@
     class="overflow-y-auto justify-center flex"
   >
     <div
-      class="grid grid-flow-col lg:grid-cols-6 py-10 grid-cols-1 gap-7 h-screen justify-center snap-parent xl:w-[60%] lg:w-[70%] md:w-[80%] sm:w-[90%] xs:w-[100%]"
+      class="grid grid-flow-col xl:grid-cols-6 py-10 grid-cols-1 gap-7 h-screen justify-center snap-parent xl:w-[60%] lg:w-[70%] md:w-[80%] sm:w-[90%] xs:w-[100%]"
     >
-      <div class="lg:col-span-4 gap-7 flex flex-col pb-10">
+      <div class="xl:col-span-4 gap-7 flex flex-col pb-10">
         <PostsView />
       </div>
       <div
-        class="sticky hidden lg:block h-min border border-gray-200 col-span-2 rounded-xl p-4 bg-white"
+        class="sticky hidden xl:block h-min border border-gray-200 col-span-2 rounded-xl p-4 bg-white"
       >
         <LeaderboardView />
       </div>
