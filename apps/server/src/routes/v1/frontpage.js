3ce21c15097d83145e40227c04849a91503988f4
Panjkrc <panjkrc.mihael@gmail.com>
2022-06-06 19:33:55 +0200

    Update frontpage.js

--- apps/server/src/routes/v1/frontpage.js
+++ apps/server/src/routes/v1/frontpage.js
@@ -3,32 +3,33 @@ let Idea = require("@/database/models/idea");
 let Comment = require("@/database/models/idea");
 
 module.exports = (router) => {
-  router.get("/frontpage-leaderboard", async (req, res) => {
-    let ideas = await Idea.find({});
+	router.get("/frontpage-leaderboard", async (req, res) => {
+		let ideas = await Idea.find({});
 
-    let users = await User.find({
-      'account.company_id': req.user.account.company_id,
-    }).select("-password");
+		let users = await User.find({
+			'account.company_id': req.user.account.company_id,
+			is_admin: false
+		}).select("-password");
 
-    let comments = await Comment.find({ created_by: req.user._id });
+		let comments = await Comment.find({ created_by: req.user._id });
 
-    users.forEach((user) => {
-      let userIdeas = ideas.filter(
-        (idea) => String(idea.created_by) == String(user._id)
-      );
-      let likes = 0;
-      userIdeas.forEach((idea) => {
-        likes += idea.likes.length;
-      });
-      user.likes = likes;
-    });
+		users.forEach((user) => {
+			let userIdeas = ideas.filter(
+				(idea) => String(idea.created_by) == String(user._id)
+			);
+			let likes = 0;
+			userIdeas.forEach((idea) => {
+				likes += idea.likes.length;
+			});
+			user.likes = likes;
+		});
 
-    users.sort((a, b) => {
-      return b.likes - a.likes;
-    });
+		users.sort((a, b) => {
+			return b.likes - a.likes;
+		});
 
-    res.send(users.slice(0, 3));
-  });
+		res.send(users.slice(0, 3));
+	});
 
-  return router;
+	return router;
 };
